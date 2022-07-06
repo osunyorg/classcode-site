e07a1c5d9acff0ee1833dc8a40507c60dc4768c2
alexisben <alexiben7@gmail.com>
2022-07-06 20:48:23 +0200

    upgrade

--- postcss.config.js
+++ postcss.config.js
@@ -7,20 +7,19 @@ var devPlugins = {},
         },
         '@fullhuman/postcss-purgecss': {
             content: [
+                // './hugo_stats.json',
                 './themes/**/*.html',
                 'layouts/**/*.html'
             ],
+            // defaultExtractor: (content) => {
+            //     let els = JSON.parse(content).htmlElements;
+            //     return els.tags.concat(els.classes, els.ids);
+            // },
             safelist: {
                 standard: [
                     'show',
                     'active',
                     'collapsed',
-                    'h1',
-                    'h2',
-                    'h3',
-                    'h4',
-                    'h5',
-                    'h6',
                     /^dropdown/,
                     /^nav-level-/,
                     /^splide_/,
@@ -40,8 +39,6 @@ var devPlugins = {},
                     'gzoomOut'
                 ],
                 deep: [
-                    /block-/,
-                    /page-/,
                     // Glightbox
                     /^glightbox/,
                     /^gslide/,
@@ -65,7 +62,9 @@ var devPlugins = {},
                     /__page/,
                     /__section/,
                     /__term/,
-                    /call_to_action/
+                    /posts/,
+                    /call_to_action/,
+                    /block-timeline/
                 ]
             }
         }
